35dc7507a25bfad07a19882c7c9bf24803d5c4b9
Félix Francisco Enríquez Romero <ffer200395@gmail.com>
2024-02-24 18:33:53 +0000

    First commit

--- UnirPDFs.py
+++ UnirPDFs.py
@@ -1,12 +1,51 @@
-#Importamos streamlit
-
 import streamlit as st 
-
-#importamos biblioteca de PDFs
-#Félix
-
 import PyPDF2
 
+# Función de verificación de credenciales
+def verificar_credenciales(usuario, password):
+    # Cargo la lista de usuarios y contraseñas
+    ls_users = st.secrets["USERS"]
+    ls_pwd = st.secrets["PWD"]
+    # Compruebo si el usuario existe en la bbdd
+    if usuario in ls_users:
+        idx_user = ls_users.index(usuario)
+        valid_pwd = ls_pwd[idx_user]
+        return valid_pwd==password
+    return False
+
+# Interfaz de Streamlit
+def main():
+    # Título
+    st.title("Sistema de XXX con Autenticación")
+
+    # Verificar si el usuario está autenticado
+    if 'autenticado' not in st.session_state or not st.session_state['autenticado']:
+        # Solicitar usuario y contraseña
+        usuario = st.text_input("Nombre de usuario")
+        password = st.text_input("Contraseña", type="password")
+
+        # Botón para iniciar sesión
+        if st.button("Iniciar sesión"):
+            # Verificación de credenciales
+            if verificar_credenciales(usuario, password):
+                st.session_state['autenticado'] = True
+                st.session_state['usuario'] = usuario
+                st.success("Has iniciado sesión correctamente.")
+            else:
+                st.error("Credenciales incorrectas.")
+    else:
+        # Usuario autenticado
+        st.success(f"Bienvenido de nuevo, {st.session_state['usuario']}")
+
+        # Contenido protegido (ejemplo)
+        st.markdown("## Aquí va el contenido protegido de la aplicación de facturación")
+
+        # Botón de cierre de sesión
+        if st.button("Cerrar sesión"):
+            del st.session_state['autenticado']
+            del st.session_state['usuario']
+            st.experimental_rerun()
+"""
 output_pdf = "documents/pdf_final.pdf"
 
 def unir_pdfs(output_path, documents):
@@ -33,3 +72,4 @@ if unir:
         with open(output_pdf, 'rb') as file:
             pdf_data=  file.read()
         st.download_button(label="Descargar PDF final", data=pdf_data, file_name="pdf_final.pdf")
+"""
